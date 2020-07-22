2aa230f8f1e06d766a818e43964f0ebeabc959a8
khushdt <khushdt@gmail.com>
2020-07-22 14:01:51 +0530

    message setup
    Signed-off-by: khushdt <khushdt@gmail.com>

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
+const path = require('path') ;
+const http = require('http') ;
 const express = require('express') ;
+const socketio = require('socket.io') ;
 
 const app = express() ;
+const server = http.createServer(app) ;
+const io = socketio(server) ; 
 
-const PORT = 3000 || process.env.PORT ;
+//set static folder
+app.use(express.static(path.join(__dirname, 'public'))); 
 
-app.listen(PORT , ()=> console.log('Server is running on ${PORT} ')) ;
+// run when a client connects
+
+io.on('connection' , socket => {
+    
+    //welcome new user
+    socket.emit('message' , 'Welcome to FastChat') ;
+    //when a user connects
+    socket.broadcast.emit('message' , 'a user has joined the chat');
+
+    // when disconnecting
+    socket.on('disconnect' , ()=> {
+        io.emit('message' , 'a user has left the chat') ;
+    }) ;
+ 
+    // listern for chatmessage
+    socket.on('chatMessage' , msg => {
+        io.emit('message' , msg) ;
+    }) ;
+
+
+}) ;
+
+const PORT = 3000  ;
+
+server.listen(PORT , ()=> console.log('Server is running on 3000 ')) ;
